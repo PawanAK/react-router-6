8602764c3c544436273fba1b3b48fddbe354f7b6
PawanAK <pawanajjark@gmail.com>
2023-05-15 20:46:31 +0530

    Route,Routes,Path,NAv bar understood

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,18 @@ import Error from "./pages/Error";
 import Login from "./pages/Login";
 import SingleProduct from "./pages/SingleProduct";
 import Products from "./pages/Products";
+import SharedLayout from "./pages/SharedLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="products" element={<Products />} />
-        <Route path="*" element={<Error />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
