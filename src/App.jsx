e3cb1e694fc6f92eec5bd2281e78515abe1a90f8
PawanAK <pawanajjark@gmail.com>
2023-05-15 20:11:32 +0530

    Navigating through routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import DashBoard from "./pages/Dashboard";
+import Error from "./pages/Error";
+import Login from "./pages/Login";
+import SingleProduct from "./pages/SingleProduct";
+import Products from "./pages/Products";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
-        <Route path="testing" element={<div>test Page</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
